c6bebbed83294127eb1663c6de0bdac8b83b5c53
Андрей SAD <sinanddex@gmail.com>
2024-01-09 17:56:52 +0200

    Работа с деструктором и конструктором

--- SoloLearnModule/Module.cpp
+++ SoloLearnModule/Module.cpp
@@ -1,41 +1,22 @@
 #include <iostream>
+#include "Header.h"
 using namespace std;
 
-class MyClass {
-private:
-	int var;
+MyClass::MyClass()
+{
+	cout << "Constructor" << endl;
+}
 
-public:
-
-	MyClass() {}
-	MyClass(int a) : var(a) {}
-	
-	friend MyClass operator+(MyClass& a1, MyClass& a2)
-	{
-		MyClass res;
-		res.var = a1.var + a2.var;
-		return res;
-	}
-	
-	int getVar()
-	{
-		return var;
-	}
-
-
-};
-
+MyClass::~MyClass()
+{
+	cout << "Destructor" << endl;
+}
 
 int main()
 {
 	setlocale(LC_ALL, "Ru");
-
-	MyClass a1(12), a2(13);
-	MyClass res = (a1 + a2);
-	MyClass res;
-	cout << res.getVar();
+	MyClass q;
 
 	return 0;
 }
 
-// Добавление комментария
