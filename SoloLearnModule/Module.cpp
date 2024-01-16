f107041874b06037ef8e4f4971d9b4b6b8422fe1
Андрей <sinanddex@gmail.com>
2024-01-16 10:59:03 +0200

    Композиция классов

--- SoloLearnModule/Module.cpp
+++ SoloLearnModule/Module.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "Header.h"
+#include <string>
 using namespace std;
 
 void Birthday::printDate()
@@ -7,11 +8,27 @@ void Birthday::printDate()
 	cout << day << "/" << month << "/"  << year << endl;
 }
 
+class Person {
+public:
+	Person(string n, Birthday b) : name(n), bd(b)
+	{
+	}
+	void printInfo()
+	{
+		cout << name << endl;
+		bd.printDate();
+	}
+private:
+	string name;
+	Birthday bd;
+};
+
 int main()
 {
 	setlocale(LC_ALL, "Ru");
-	Birthday Jim(01, 02, 03);
-	Jim.printDate();
+	Birthday bd(2, 21, 1985);
+	Person p("David", bd);
+	p.printInfo();
 	return 0;
 }
 
