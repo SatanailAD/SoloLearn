f0edb8c7af6ae4dd3fc11f55b315e2c9b4819741
Андрей <sinanddex@gmail.com>
2024-01-29 10:33:10 +0200

    Task 1

--- SoloLearnModule/Module.cpp
+++ SoloLearnModule/Module.cpp
@@ -1,55 +1,23 @@
 #include <iostream>
 #include <string>
+using namespace std;
+
+//Дано число. Проверьте, отрицательное оно или нет. Выведите об этом информацию в консоль.
 
 int main() {
-    std::string myString;
-    std::cin >> myString;
-
-    char charArray[myString.length() + 1];
-
-    std::copy(myString.begin(), myString.end(), charArray);
-
-    charArray[myString.length()] = '\0';
-
-    std::cout << charArray << std::endl;
-
+    
+    double(x);
+    cout << "Enter ur number: ";
+    cin >> x;
+    if (x>=0)
+    {
+        cout << "Your number is positive"<<endl;
+    }
+    else
+    {
+        cout << "Your number is negative" << endl;
+    }
     return 0;
 }
 
 
-/*
-class FriendClass {
-private:
-    int privateData;
-
-public:
-    FriendClass() : privateData(0) {}
-
-    
-    // Дружественная функция
-    friend void friendFunction(FriendClass obj);
-
-    // Дружественный класс
-    friend class AnotherFriendClass;
-};
-
-// Дружественная функция
-void friendFunction(FriendClass obj) {
-    std::cout << "Private data: " << obj.privateData << std::endl;
-}
-
-// Дружественный класс
-class AnotherFriendClass {
-public:
-    void accessPrivateData(FriendClass obj) {
-        // Дружественный класс имеет доступ к закрытым членам
-        std::cout << "Private data from AnotherFriendClass: " << obj.privateData << std::endl;
-    }
-};
-
-int main()
-{
-    
-}
-
-*/
