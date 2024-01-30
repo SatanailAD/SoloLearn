7c3d21f5539d315199daeafe068b3b72b2d868d1
Андрей <sinanddex@gmail.com>
2024-01-30 16:49:08 +0200

    Проверьте, что это число находится в диапазоне от 1 до 100

--- SoloLearnModule/Module.cpp
+++ SoloLearnModule/Module.cpp
@@ -2,14 +2,21 @@
 #include <string>
 using namespace std;
 
-//Преобразуйте число в строку:
+//Проверьте, что это число находится в диапазоне от 1 до 100
 
 int main() {
     
     setlocale(LC_ALL, "Rus");
-    int num(123);
-    string mes{to_string(num)};
-    cout << mes;
+    int num(100);
+    if (num > 0 && num <= 100)
+    {
+        cout << "OK";
+    }
+    else
+    {
+        cout << "Not OK";
+    }
+    
     return 0;
 }
 
