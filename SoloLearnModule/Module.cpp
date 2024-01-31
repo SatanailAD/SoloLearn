3097c329ec8fd8d6a4814ebf31c2898e82753df9
Андрей <sinanddex@gmail.com>
2024-01-31 14:20:24 +0200

    //Дано целое число. Выведите в консоль последнюю цифру этого числа. //Дано целое число.Выведите в консоль первую цифру этого числа. //Дано целое число.Выведите в консоль сумму первой и последней цифры этого числа.

--- SoloLearnModule/Module.cpp
+++ SoloLearnModule/Module.cpp
@@ -2,21 +2,32 @@
 #include <string>
 using namespace std;
 
-//Проверьте, что это число находится в диапазоне от 1 до 100
+//Дано целое число. Выведите в консоль последнюю цифру этого числа.
+//Дано целое число.Выведите в консоль первую цифру этого числа.
+//Дано целое число.Выведите в консоль сумму первой и последней цифры этого числа.
 
 int main() {
     
     setlocale(LC_ALL, "Rus");
-    int num(100);
-    if (num > 0 && num <= 100)
+    int num;
+    int firstNumber, lastNumber;
+    int amountNum = 0;
+    cout << "Etner a number: ";
+    cin >> num;
+    int tempNum = num;
+    while (tempNum != 0)
     {
-        cout << "OK";
+        firstNumber = tempNum % 10;
+        tempNum /= 10;
+        amountNum++;
     }
-    else
-    {
-        cout << "Not OK";
-    }
-    
+    lastNumber = num % 10;
+    cout << "Ваше число: " << num << endl;
+    cout << "Первый символ: " << firstNumber;
+    cout << "Последний символ: " << lastNumber << endl;
+    cout << "Количество символов: " << amountNum << endl;
+    cout << "Сумма первого и последнего числа: " << firstNumber + lastNumber << endl;
+
     return 0;
 }
 
